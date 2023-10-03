2bad62235a0ffa6dd75a1850904adaa0c8bfcfd9
sp1603 <sahithya.pasagada@gmail.com>
2023-10-03 01:37:50 -0400

    fix js elements

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let id = 1;
-let showingMoves = false;
+
 async function getPokemonData() {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const data = await response.json();
+  //console.log(data);
+  //console.log(data.stats)
   const pokemonName = data.name;
   const pokemonImage = data.sprites.front_default;
   const pokemonTypes = data.types;
@@ -10,45 +12,51 @@ async function getPokemonData() {
   const pokemonMoves = data.moves;
   const pokemonHeight = data.height;
   const pokemonWeight = data.weight;
+  showName(pokemonName);
   showImage(pokemonImage);
+  showTypes(pokemonTypes);
+  showInfo(pokemonStats, pokemonHeight, pokemonWeight);
+  showMoves(pokemonMoves);
  
 }
 
+function showName(pokemonName) {
+  const textName = document.getElementById("name");
+  textName.textContent = pokemonName;
+}
 
 function showImage(pokemonImage) {
   document.getElementById("image").src = pokemonImage;
 }
 
-function showMoves(pokemonMoves) {
-  let moves = [];
-  for e
+function showTypes(pokemonTypes) {
+  let categories = document.getElementById("types");
+  let content = "";
+  for (const elt of pokemonTypes) {
+    content += `<p id="${elt.type.name}">${elt.type.name}</p>\n`
+  }
+  categories.innerHTML = content;
 }
 
+function showInfo(pokemonStats, pokemonHeight, pokemonWeight) {
+  let height = (pokemonHeight / 10).toFixed(1);
+  let weight = (pokemonWeight / 10).toFixed(1);
+  let pokemonStatData = document.getElementById("info");
 
+  pokemonStatData.innerHTML = `
+        <p>height: ${height}m</p>
+        <p>weight: ${weight}kg</p>
+        <p>hp: ${pokemonStats[0]["base_stat"]}</p>
+        <p>attack: ${pokemonStats[1]["base_stat"]}</p>
+        <p>defense: ${pokemonStats[2]["base_stat"]}</p>
+        <p>special-attack: ${pokemonStats[3]["base_stat"]}</p>
+        <p>special-defense: ${pokemonStats[4]["base_stat"]}</p>
+        <p>speed: ${pokemonStats[5]["base_stat"]}</p>
+  `;
+}
 
+function showMoves(pokemonMoves) {
 
-
-
-
-const colors = {
-  normal: "#A8A77A",
-  fire: "#EE8130",
-  water: "#6390F0",
-  electric: "#F7D02C",
-  grass: "#7AC74C",
-  ice: "#96D9D6",
-  fighting: "#C22E28",
-  poison: "#A33EA1",
-  ground: "#E2BF65",
-  flying: "#A98FF3",
-  psychic: "#F95587",
-  bug: "#A6B91A",
-  rock: "#B6A136",
-  ghost: "#735797",
-  dragon: "#6F35FC",
-  dark: "#705746",
-  steel: "#B7B7CE",
-  fairy: "#D685AD"
-};
+}
 
 getPokemonData();
